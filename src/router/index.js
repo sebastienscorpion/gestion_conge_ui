10339892da85a39aad9433271dba95d74f945012
sebastienscorpion <sebastienmukombe.com>
2024-01-23 20:07:05 -0500

    add an others page in view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import employes from '../views/employes.vue'
+import Listconge from '../views/Listconge.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +13,16 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/employes',
+      name: 'employes',
+      component: employes
+    },
+    {
+      path: '/Listconge',
+      name: 'Listconge',
+      component: Listconge
+    },
+   
   ]
 })
 
